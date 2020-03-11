1bc91c9f20ef2d28b28df915054eaf5e0a5aa2cc
Dmitriy <kaishien777@gmail.com>
2020-03-11 18:50:50 +0300

    Added navigation component

--- blog/App.js
+++ blog/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import { AppLoading } from "expo";
 import { bootstrap } from "./bootstrap";
+import { AppNavigation } from "./navigation/AppNavigation";
 
 
 export default function App() {
@@ -17,9 +18,5 @@ export default function App() {
     )
   }
 
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <AppNavigation/>
 }
