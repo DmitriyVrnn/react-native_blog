34f86d99c2537e45ec42b343c88f9c3c9edcdf03
Dmitriy <kaishien777@gmail.com>
2020-03-15 01:22:20 +0300

    Alert window on remove post

--- blog/src/screens/PostScreen.js
+++ blog/src/screens/PostScreen.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, Button, ScrollView, Alert } from 'react-native';
+import { THEME } from "../theme";
+import { DATA } from "../data";
+
 
 export const PostScreen = ({ route }) => {
-  return(
-    <View style={styles.center}>
-      <Text>{route.params.postId}</Text>
-    </View>
+  const post = DATA.find(post => post.id === route.params.postId);
+
+  const removeHandler = () => {
+    Alert.alert(
+      'Удаление поста',
+      'Вы уверены?',
+      [
+        {
+          text: 'Отменить',
+          style: 'cancel',
+        },
+        {text: 'Удалить', style: 'destructive', onPress: () => {}},
+      ],
+      {cancelable: false},
+    );
+  };
+
+  return (
+    <ScrollView>
+      <Image source={{ uri: post.img }} style={styles.image}/>
+      <View style={styles.textWrap}>
+        <Text style={styles.postTitle}>{post.text}</Text>
+      </View>
+      <Button title="Удалить" color={THEME.DANGER_COLOR} onPress={removeHandler}/>
+    </ScrollView>
   )
 };
 
@@ -14,5 +38,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  textWrap: {
+    padding: 10,
+  },
+  postTitle: {
+    fontFamily: 'open-regular'
   }
 });
