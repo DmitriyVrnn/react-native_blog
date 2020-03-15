2860bfa9ef365e45dad021666f7a40504be0d93e
Dmitriy <kaishien777@gmail.com>
2020-03-15 03:07:31 +0300

    Bottom tab navigation

--- blog/src/navigation/AppNavigation.js
+++ blog/src/navigation/AppNavigation.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { AppHeaderIcon } from "../components/AppHeaderIcon";
-import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack";
 import { Platform } from 'react-native';
@@ -8,59 +7,112 @@ import { MainScreen } from "../screens/MainScreen";
 import { PostScreen } from "../screens/PostScreen";
 import { THEME } from "../theme";
 import { HeaderButtonsApp } from "../components/HeaderButtonsApp";
+import { BookedScreen } from "../screens/BookedScreen";
+import { CreateScreen } from "../screens/CreateScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 
 const Stack = createStackNavigator();
 
+const BottomNavigator = createBottomTabNavigator();
+
+const StackPostNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Blog"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff',
+        },
+        headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR,
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      <Stack.Screen
+        name="Blog"
+        component={MainScreen}
+        options={{
+          headerRight: () => (
+            <HeaderButtonsApp
+              onPress={() => console.log('Press')}
+              iconName='ios-camera'
+              title='Take photo'/>),
+          headerLeft: () => (
+            <HeaderButtonsApp
+              onPress={() => console.log('Press')}
+              iconName='ios-menu'
+              title='Take photo'/>)
+        }}
+      />
+      <Stack.Screen
+        options={({ route }) => ({
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          title: `Пост от ${new Date(route.params.postDate).toLocaleDateString()}`,
+          headerRight: () => (
+            <HeaderButtonsApp
+              onPress={() => console.log('Press')}
+              iconName={route.params.booked ? 'ios-star' : 'ios-star-outline'}
+              title='Booked'/>)
+        })}
+        name="Post"
+        component={PostScreen}
+      />
+    </Stack.Navigator>
+  )
+};
+
+const StackBookedNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Blog"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff',
+        },
+        headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR,
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      <Stack.Screen
+        name="Booked"
+        component={BookedScreen}
+      />
+    </Stack.Navigator>
+  )
+};
+
 export const AppNavigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Blog"
-                       screenOptions={{
-                         headerStyle: {
-                           backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff',
-                         },
-                         headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR,
-                         headerTitleStyle: {
-                           fontWeight: 'bold',
-                         },
-                       }}>
-        <Stack.Screen
-          name="Blog"
-          component={MainScreen}
+      <BottomNavigator.Navigator
+        tabBarOptions={{
+          activeTintColor: THEME.MAIN_COLOR,
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <BottomNavigator.Screen
+          name="Post"
+          component={StackPostNavigator}
           options={{
-            headerRight: () => (
-              <HeaderButtonsApp
-                onPress={() => console.log('Press')}
-                iconName='ios-camera'
-                title='Take photo'/>),
-            headerLeft: () => (
-              <HeaderButtonsApp
-                onPress={() => console.log('Press')}
-                iconName='ios-menu'
-                title='Take photo'/>)
+            tabBarIcon: (info) => <Ionicons name='ios-albums' size={25} color={info.color}/>,
           }}
         />
-        <Stack.Screen
-          options={({ route }) => ({
-            headerStyle: {
-              backgroundColor: '#f4511e',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-            title: `Пост от ${new Date(route.params.postDate).toLocaleDateString()}`,
-            headerRight: () => (
-              <HeaderButtonsApp
-                onPress={() => console.log('Press')}
-                iconName={route.params.booked ? 'ios-star' : 'ios-star-outline'}
-                title='Booked'/>)
-          })}
-          name="Post"
-          component={PostScreen}
+        <BottomNavigator.Screen
+          name="Booked"
+          component={StackBookedNavigator}
+          options={{
+            tabBarIcon: (info) => <Ionicons name='ios-star' size={25} color={info.color}/>,
+          }}
         />
-      </Stack.Navigator>
+      </BottomNavigator.Navigator>
     </NavigationContainer>
   )
 };
