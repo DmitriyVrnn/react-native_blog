d8cc52c68f6f6db62dc2d79c2e41896e42e83ddf
Dmitriy <kaishien777@gmail.com>
2020-03-15 22:24:03 +0300

    Styles for left navigation

--- blog/src/navigation/AppNavigation.js
+++ blog/src/navigation/AppNavigation.js
@@ -4,16 +4,43 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import { BottomNavigation } from "./BottomNavigation";
 import { StackAboutNavigator, StackCreateNavigator } from "./StackNavigator";
+import { THEME } from "../theme";
 
 const MainNavigator = createDrawerNavigator();
 
 export const AppNavigation = () => {
   return (
     <NavigationContainer>
-      <MainNavigator.Navigator>
-        <MainNavigator.Screen name="Posts" component={BottomNavigation} />
-        <MainNavigator.Screen name="About" component={StackAboutNavigator} />
-        <MainNavigator.Screen name="Create" component={StackCreateNavigator} />
+      <MainNavigator.Navigator
+        drawerContentOptions={{
+          activeTintColor: THEME.MAIN_COLOR,
+          itemStyle: { marginVertical: 10 },
+          labelStyle: {
+            fontFamily: 'open-bold',
+          }
+        }}
+      >
+        <MainNavigator.Screen
+          options={{
+            drawerLabel: 'Главная',
+          }}
+          name="Posts"
+          component={BottomNavigation}
+        />
+        <MainNavigator.Screen
+          name="About"
+          component={StackAboutNavigator}
+          options={{
+            drawerLabel: 'О приложении'
+          }}
+        />
+        <MainNavigator.Screen
+          name="Create"
+          component={StackCreateNavigator}
+          options={{
+            drawerLabel: 'Новый пост'
+          }}
+        />
       </MainNavigator.Navigator>
     </NavigationContainer>
   )
