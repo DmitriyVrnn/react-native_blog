d25cf01c4abe9465db14296b8282b9031e80b51e
Dmitriy <kaishien777@gmail.com>
2020-03-19 16:59:59 +0300

    Connected SQLite database

--- blog/src/bootstrap.js
+++ blog/src/bootstrap.js
@@ -1,8 +1,16 @@
 import * as Font from 'expo-font';
+import { Database } from "./db";
+
 
 export async function bootstrap() {
-  await Font.loadAsync({
-    'open-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-    'open-regular': require('./assets/fonts/OpenSans-Regular.ttf'),
-  })
+  try {
+    await Font.loadAsync({
+      'open-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+      'open-regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+    });
+    await Database.init();
+    console.log('Database started...')
+  } catch (e) {
+    console.log(e);
+  }
 }
